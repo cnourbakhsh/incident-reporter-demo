ee96f0f067f8b51aaad5dcdd4d421b133d879f57
cnourbakhsh <cnourbakhsh@vizuri.com>
2017-12-05 11:04:41 -0500

    Added build folder dist to source control

--- services/src/environments/environment.prod.ts
+++ services/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  host: 'http://process-server-incident-demo.192.168.99.100.nip.io',
+  auth_header: 'Basic cHJvY2Vzc29yOnByb2Nlc3NvciM5OQ==',
+  containerId: '1776e960572610314f3f813a5dbb736d',
+  metadataUrlChunk: '/bpm/kie-server/services/rest/server/queries/processes/instances/',
+  metadataUrlChunkParams: {
+    withVars: true
+  },
+  imagesUrlChunk: '/bpm/kie-server/services/rest/server/containers/',
+  imagesUrlChunkWithProcesses: '/images/processes/instances/'
 };
