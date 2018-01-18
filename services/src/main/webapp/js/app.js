b9a02cf131595f486e0be5844cb34c304c07b64c
cnourbakhsh <cnourbakhsh@vizuri.com>
2018-01-18 12:54:25 -0500

    Added user to params for PUT to services server to processes server

--- services/src/main/webapp/js/app.js
+++ services/src/main/webapp/js/app.js
@@ -1,10 +1,11 @@
-(function() {
+(function () {
 	'use strict';
 
-	angular.module('bpmsFlowApp', [ 'bpmsFlowApp.controllers' ]);
+	angular.module('bpmsFlowApp', ['bpmsFlowApp.controllers']);
 
-	angular.module('bpmsFlowApp').config([ '$httpProvider', function($httpProvider) {
+	angular.module('bpmsFlowApp').config(['$httpProvider', function ($httpProvider) {
+		// Basic auth formed from KIE_SERVER_USER and KIE_SERVER_PASSWORD environment variables in Process server
 		$httpProvider.defaults.headers.common['Authorization'] = 'Basic cHJvY2Vzc29yOnByb2Nlc3NvciM5OQ==';
-	} ]);
+	}]);
 
 })();
--- services/src/main/webapp/js/controllers.js
+++ services/src/main/webapp/js/controllers.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
 	'use strict';
 
 	angular.module('bpmsFlowApp.controllers', []);
@@ -19,7 +19,7 @@
 
 		vm.approvalComments = 'Approval from supervisor app!';
 		vm.remediationAccepted = true;
-		
+
 		vm.load = load;
 		vm.approve = approve;
 
@@ -31,19 +31,19 @@
 		function loadVars() {
 			$log.info('loadVars called');
 			$http({
-				method : 'GET',
-				withCredentials : true,
-				url : location.protocol + '//' + location.host + '/services-0.0.1-SNAPSHOT/bpm/kie-server/services/rest/server/queries/processes/instances/'+vm.processId+'?withVars=true',
+				method: 'GET',
+				withCredentials: true,
+				url: location.protocol + '//' + location.host + '/services-0.0.1-SNAPSHOT/bpm/kie-server/services/rest/server/queries/processes/instances/' + vm.processId + '?withVars=true',
 				headers: {
-					   'Accept': 'application/json'
-					 }
+					'Accept': 'application/json'
+				}
 			}).then(function successCallback(response) {
 				var data = response.data;
-				
+
 				vm.comments = [];
 				vm.photos = [];
 				vm.completed = false;
-				
+
 				if (data["process-instance-variables"]["incidentPhotoIds"]) {
 					var photoIdArr = data["process-instance-variables"]["incidentPhotoIds"];
 					photoIdArr = JSON.parse(photoIdArr.replace(/'/g, '"'));
@@ -52,22 +52,22 @@
 						vm.photos.push(location.protocol + '//' + location.host + '/services-0.0.1-SNAPSHOT/photos/' + vm.processId + '/' + photoName);
 					}
 				}
-				
+
 				if (data["process-instance-variables"]["incidentComments"]) {
 					var commentArr = data["process-instance-variables"]["incidentComments"];
 					commentArr = JSON.parse(commentArr.replace(/'/g, '"'));
 					vm.comments = commentArr;
-				} 
-				
+				}
+
 				if (data["process-instance-variables"]["incidentStatus"]) {
 					var processStatus = data["process-instance-variables"]["incidentStatus"];
 					$log.info('Status: ', processStatus);
-					
+
 					if (processStatus === 'COMPLETED') {
 						vm.completed = true;
 					}
 				}
-				
+
 			}, function errorCallback(error) {
 				vm.comments = [];
 				vm.photos = [];
@@ -79,23 +79,23 @@
 			var targetUrl = location.protocol + '//' + location.host + '/services-0.0.1-SNAPSHOT/bpm/kie-server/services/rest/server/containers/' + vm.containerId + "/images/processes/instances/" + vm.processId;
 			$log.info('Calling: ' + targetUrl);
 			$http({
-				method : 'GET',
-				withCredentials : true,
-				url : targetUrl
+				method: 'GET',
+				withCredentials: true,
+				url: targetUrl
 			}).then(function successCallback(response) {
 				$log.info('Successful loadImage call: ' + response.status)
 				var raw = response.data.substr(response.data.indexOf('svg') - 1);
 
 				var wrapper = document.getElementById('image');
 				wrapper.innerHTML = raw;
-				
+
 				var svg = document.getElementsByTagName('svg')[0];
-				
+
 				svg.setAttribute('width', '900px');
 				svg.setAttribute('height', '600px');
 				svg.setAttribute('preserveAspectRatio', 'xMinYMid meet');
 				svg.setAttribute('viewBox', '5 275 1500 500');
-				
+
 			}, function errorCallback(error) {
 				var wrapper = document.getElementById('image');
 				wrapper.innerHTML = '<i>No process image found</i>';
@@ -107,12 +107,12 @@
 			var baseUrl = location.protocol + '//' + location.host + '/services-0.0.1-SNAPSHOT/bpm/kie-server/services/rest/server/';
 			$log.info('Approving');
 			$http({
-				method : 'GET',
-				withCredentials : true,
-				url : baseUrl + 'queries/tasks/instances/process/'+vm.processId+'?status=Ready',
+				method: 'GET',
+				withCredentials: true,
+				url: baseUrl + 'queries/tasks/instances/process/' + vm.processId + '?status=Ready',
 				headers: {
-					   'Accept': 'application/json'
-					 }
+					'Accept': 'application/json'
+				}
 			}).then(function successCallback(response) {
 				var data = response.data;
 				var approveTask;
@@ -123,17 +123,17 @@
 						break;
 					}
 				}
-				
+
 				if (approveTask) {
 					$http({
-						method : 'PUT',
-						withCredentials : true,
-						url : baseUrl + 'containers/'+vm.containerId+'/tasks/'+approveTask["task-id"]+'/states/completed?auto-progress=true',
+						method: 'PUT',
+						withCredentials: true,
+						url: baseUrl + 'containers/' + vm.containerId + '/tasks/' + approveTask["task-id"] + '/states/completed?auto-progress=true&user=processor',
 						headers: {
-							   'Accept': 'application/json',
-							   'Content-Type': 'application/json'
-							 },
-						data: { 'comments' : vm.approvalComments, 'accepted' : vm.remediationAccepted }
+							'Accept': 'application/json',
+							'Content-Type': 'application/json'
+						},
+						data: { 'comments': vm.approvalComments, 'accepted': vm.remediationAccepted }
 					}).then(function successCallback(response) {
 						vm.load();
 					}, function (error) {
@@ -145,8 +145,8 @@
 				$log.error('Error loadingImage: ' + error);
 			});
 		}
-		
-		$interval(function() {
+
+		$interval(function () {
 			if (vm.autoUpdate) {
 				$log.info('Updating...');
 				load();
