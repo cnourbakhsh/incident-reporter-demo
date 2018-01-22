b9b0e91c9eac025bbcb499eb3cbbd2cfab930170
cnourbakhsh <cnourbakhsh@vizuri.com>
2018-01-22 11:19:35 -0500

    Updated the services-server to supervisor-server and it's routes

--- setup/setup-project.sh
+++ setup/setup-project.sh
@@ -30,6 +30,7 @@ echo "getting nexus and jboss images"
 oc login -u system:admin
 oc create -f setup/nexus3-persistent-template.yaml -n openshift
 oc create -f setup/jboss-image-streams.json -n openshift
+oc import-image my-jboss-webserver-3/webserver31-tomcat8-openshift --from=registry.access.redhat.com/jboss-webserver-3/webserver31-tomcat8-openshift --confirm
 
 #Create the project
 echo "logging into minishift"
@@ -70,22 +71,22 @@ oc expose svc/decision-server
 oc new-app registry.access.redhat.com/jboss-processserver-6/processserver63-openshift~https://github.com/cnourbakhsh/incident-reporter-demo.git#consolidated --context-dir=processes -e KIE_SERVER_USER='processor' -e KIE_SERVER_PASSWORD='processor#99' -e KIE_SERVER_BPM_UI_DISABLED=false -e KIE_SERVER_BYPASS_AUTH_USER=true -e RESPONDER_PUSH_USER_NAME=test -e RESPONDER_PUSH_USER_SECRET=test -e RESPONDER_PUSH_ENDPOINT='http://mobile-backend-incident-demo.'$MINISHIFT_URL -e REPORTER_PUSH_USER_NAME=test -e REPORTER_PUSH_USER_SECRET=test -e REPORTER_PUSH_ENDPOINT='http://mobile-backend-incident-demo.'$MINISHIFT_URL --name=process-server
 oc expose svc/process-server
 
-#Deploy the Services Server
-oc new-app registry.access.redhat.com/jboss-webserver-3/webserver31-tomcat8-openshift~https://github.com/cnourbakhsh/incident-reporter-demo.git#consolidated --context-dir=services -e SPRING_APPLICATION_JSON='{"zuul":{"routes":{"bpm":{"url":"http://process-server-incident-demo.'$MINISHIFT_URL'", "sensitiveHeaders":"Cookie,Set-Cookie"}}},"spring":{"data":{"rest":{"base-path":"/api"}},"http":{"multipart":{"max-file-size":"10MB","max-request-size":"10MB"}}}}' --name=services-server
-oc expose svc/services-server
+#Deploy the Supervisor Server
+oc new-app registry.access.redhat.com/jboss-webserver-3/webserver31-tomcat8-openshift~https://github.com/cnourbakhsh/incident-reporter-demo.git#consolidated --context-dir=services -e SPRING_APPLICATION_JSON='{"zuul":{"routes":{"bpm":{"url":"http://process-server-incident-demo.'$MINISHIFT_URL'", "sensitiveHeaders":"Cookie,Set-Cookie"}}},"spring":{"data":{"rest":{"base-path":"/api"}},"http":{"multipart":{"max-file-size":"10MB","max-request-size":"10MB"}}}}' --name=supervisor-server
+oc expose svc/supervisor-server
 
 #Deploy the Mobile Backend
-oc new-app registry.access.redhat.com/rhscl/nodejs-4-rhel7~https://github.com/cnourbakhsh/incident-reporter-demo.git#consolidated --context-dir=backend -e DECISION_SERVER_HOST='decision-server-incident-demo.'$MINISHIFT_URL -e DECISION_CONTAINER_ID=4c1342a8827bf46033cb95f0bdf27f0b -e DECISION_BASIC_AUTH='Basic ZGVjaWRlcjpkZWNpZGVyIzk5' -e PROCESS_SERVER_HOST='process-server-incident-demo.'$MINISHIFT_URL -e PROCESS_CONTAINER_ID=1776e960572610314f3f813a5dbb736d -e PROCESS_BASIC_AUTH='Basic cHJvY2Vzc29yOnByb2Nlc3NvciM5OQ==' --name=mobile-backend -e SERVICES_SERVER_HOST='services-server-incident-demo.'$MINISHIFT_URL -e EXPOSED_SERVICES_SERVER_HOST='services-server-incident-demo.'$(ipconfig getifaddr en0) -e OPENSHIFT_NODEJS_PORT=8080 -e OPENSHIFT_NODEJS_IP=0.0.0.0
+oc new-app registry.access.redhat.com/rhscl/nodejs-4-rhel7~https://github.com/cnourbakhsh/incident-reporter-demo.git#consolidated --context-dir=backend -e DECISION_SERVER_HOST='decision-server-incident-demo.'$MINISHIFT_URL -e DECISION_CONTAINER_ID=4c1342a8827bf46033cb95f0bdf27f0b -e DECISION_BASIC_AUTH='Basic ZGVjaWRlcjpkZWNpZGVyIzk5' -e PROCESS_SERVER_HOST='process-server-incident-demo.'$MINISHIFT_URL -e PROCESS_CONTAINER_ID=1776e960572610314f3f813a5dbb736d -e PROCESS_BASIC_AUTH='Basic cHJvY2Vzc29yOnByb2Nlc3NvciM5OQ==' --name=mobile-backend -e SERVICES_SERVER_HOST='supervisor-server-incident-demo.'$MINISHIFT_URL -e EXPOSED_SERVICES_SERVER_HOST='supervisor-server-incident-demo.'$(ipconfig getifaddr en0) -e OPENSHIFT_NODEJS_PORT=8080 -e OPENSHIFT_NODEJS_IP=0.0.0.0
 oc expose svc/mobile-backend
 
 #Expose remote routes for use by mobile devices
 
 #Delete any existing remotely exposed routes
 oc delete routes exposed-mobile-backend-route
-oc delete routes exposed-services-route
+oc delete routes exposed-supervisor-route
 
 #Remote route for mobile backend
 oc expose service/mobile-backend --name=exposed-mobile-backend-route --hostname=mobile-backend-incident-demo.$(ipconfig getifaddr en0).nip.io
 
-#Remote route for services server
-oc expose service/services-server --name=exposed-services-route --hostname=services-server-incident-demo.$(ipconfig getifaddr en0).nip.io
+#Remote route for supervisor server
+oc expose service/supervisor-server --name=exposed-supervisor-route --hostname=supervisor-server-incident-demo.$(ipconfig getifaddr en0).nip.io
