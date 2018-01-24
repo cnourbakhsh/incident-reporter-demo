c78d6b18986c562e223222f843ea72a1f0900a06
Ben-Johan van der Walt <bvanderwalt@vizuri.com>
2018-01-24 08:14:16 -0500

    Update bpm flow diagram

--- setup/start_openshift.sh
+++ setup/start_openshift.sh
@@ -4,4 +4,8 @@ sudo mount --make-shared /
 docker ps
 rm -rf .kube/
 
-oc cluster up --routing-suffix=`curl http://169.254.169.254/latest/meta-data/public-ipv4`.nip.io --public-hostname=`curl http://169.254.169.254/latest/meta-data/public-hostname` 
+export EXTERNAL_IP_ADDRESS="`curl http://169.254.169.254/latest/meta-data/public-ipv4`"
+
+export EXTERNAL_HOSTNAME="`curl http://169.254.169.254/latest/meta-data/public-hostname`"
+
+oc cluster up --routing-suffix=$EXTERNAL_IP_ADDRESS.nip.io --public-hostname=$EXTERNAL_HOSTNAME 
