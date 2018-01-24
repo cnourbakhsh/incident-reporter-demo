469f7f8454663a4cfa475cc2e34701b204901705
cnourbakhsh <cnourbakhsh@vizuri.com>
2018-01-24 10:01:18 -0500

    Commented the client UI environment files

--- responder/src/services/environment.ts
+++ responder/src/services/environment.ts
@@ -3,7 +3,15 @@
 */
 
 export const environment = {
+    /* AWS Backend */
+    //mobileBackendUrl: 'http://mobile-backend-incident-demo.54.210.148.31.nip.io'
+
+    /* Exposed Minishift Backend */
     //mobileBackendUrl: 'http://mobile-backend-incident-demo.192.168.61.106.nip.io:8080'
+
+    /* Minishift Backend */
     mobileBackendUrl: 'http://mobile-backend-incident-demo.192.168.99.100.nip.io'
+
+    /* Local NodeJS Backend */
     //mobileBackendUrl: 'http://localhost:7001'
 }
